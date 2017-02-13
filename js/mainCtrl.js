c587b3037fb11a13c97234ba4021a3ba22c4c87f
dmayes403 <davidrmayes@gmail.com>
2017-02-13 00:56:01 -0700

    added functionality to subscribe/unscribe buttons

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -11,12 +11,29 @@ angular.module('myApp')
         return $scope.eyes;
       }
 
-      $scope.pushSubscriber = function(firstName, lastName){
-        $scope.newUser = {
-          'firstName': firstName,
-          'lastName': lastName
-        }
-        srvc.subscribers.push($scope.newUser);
+
+      $scope.subscriberObj = function(firstName, lastName, age, gender, state, zip){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.state = state;
+        this.zip = zip;
+      }
+
+      $scope.pushSubscriber = function(firstName, lastName, gender){
+        $scope.David = new $scope.subscriberObj(firstName, lastName, gender);
+        $scope.David = JSON.stringify($scope.David);
+        srvc.subscribers.push($scope.David);
+        localStorage.setItem(1, $scope.David);
         console.log(srvc.subscribers);
+      }
+
+      $scope.pushUnSubscriber = function(firstName, lastName, gender){
+        for(var i = 1; i <= localStorage.length; i++){
+          if(JSON.parse(localStorage.getItem(i)).firstName === firstName){
+            localStorage.removeItem(i);
+            }
+          }
       }
 })
